a2182f923b4f6e49a65559f5c8cbc98fff212e86
zbrow <zbrow@100.64.47.239>
2022-05-21 15:10:10 -0700

    update curr version

--- TritonHacks2022/src/Fish.java
+++ TritonHacks2022/src/Fish.java
@@ -27,16 +27,16 @@ import javax.swing.JPanel;
 
 public class Fish {
 	Random rnd = new Random();
-	double x = 300; 
-	double y = 200 ; 
-	double vx = 3; 
-	double vy = 3; 
-	double accx = -.1; 
-	double accy = -.1; 
+	double x = 300;
+	double y = 200;
+	double vx = 3;
+	double vy = 3;
+	double accx = -.1;
+	double accy = -.1;
 	private AffineTransform tx;
-	private Image img; 	
-	int width; 
-	int height; 
+	private Image img;
+	int width;
+	int height;
 	static int numFish = 0;
 	static int numType1 = 0;
 	static int numType2 = 0;
@@ -44,107 +44,100 @@ public class Fish {
 	double scale;
 //	boolean right = true; 
 //	boolean down = true; 
-	
+
 	public Fish(int x, int y, int type) {
-		switch(type){
-		case 1: img = getImage("/imgs/lilFish.png" ); 
-				numType1++;
-				scale = 0.05;
-				width  = img.getWidth(null);
-				height = img.getHeight(null);
-			 break;
-		case 2: img = getImage("/imgs/tropicalFish.png" ); 
-				numType2++;
-				scale = 0.2;
-				width  = img.getWidth(null);
-				height = img.getHeight(null);
-			 break;
-		case 3: img = getImage("/imgs/shark.png" ); 
-				numType3++;
-				scale = 0.5;
-				width  = img.getWidth(null);
-				height = img.getHeight(null);
-			 break;
-		default: img = getImage("/imgs/tropicalFish.png" ); 
-				scale = 0.2;
-				width  = img.getWidth(null);
-				height = img.getHeight(null);
+		switch (type) {
+		case 1:
+			img = getImage("/imgs/lilFish.png");
+			numType1++;
+			scale = 0.05;
+			width = img.getWidth(null);
+			height = img.getHeight(null);
+			break;
+		case 2:
+			img = getImage("/imgs/tropicalFish.png");
+			numType2++;
+			scale = 0.2;
+			width = img.getWidth(null);
+			height = img.getHeight(null);
+			break;
+		case 3:
+			img = getImage("/imgs/shark.png");
+			numType3++;
+			scale = 0.5;
+			width = img.getWidth(null);
+			height = img.getHeight(null);
+			break;
+		default:
+			img = getImage("/imgs/tropicalFish.png");
+			scale = 0.2;
+			width = img.getWidth(null);
+			height = img.getHeight(null);
 		}
 		numFish++;
-		tx = AffineTransform.getTranslateInstance(x, y ); 
+		tx = AffineTransform.getTranslateInstance(x, y);
 		init(x, y);
-			}
-	
-	
-	
-	
-	
-	
-	
+	}
+
 	Timer move = new Timer(1000, new ActionListener() {
-        @Override
+		@Override
 
-        public void actionPerformed(ActionEvent arg0) {
-        	vx = rnd.nextInt((3 + 3 ) + 1) - 3 ; // [-20, 20]
-    		vy = rnd.nextInt((3 + 3 ) + 1) - 3 ; // [-20, 20]
+		public void actionPerformed(ActionEvent arg0) {
+			vx = rnd.nextInt((3 + 3) + 1) - 3; // [-20, 20]
+			vy = rnd.nextInt((3 + 3) + 1) - 3; // [-20, 20]
 
-        }
-    });
-	
-	
-	public void move() { 
+		}
+	});
+
+	public void move() {
 		move.setInitialDelay(0);
-		move.start(); 
-		move.setRepeats(true); 
-	
-	} 
+		move.start();
+		move.setRepeats(true);
+
+	}
+
 	public void paint(Graphics g) {
-		//these are the 2 lines of code needed draw an image on the screen
+		// these are the 2 lines of code needed draw an image on the screen
 		Graphics2D g2 = (Graphics2D) g;
-		
-		x += vx; 
-		y += vy; 
-		
-		
+
+		x += vx;
+		y += vy;
+
 //		vx = rnd.nextInt((3 + 3 ) + 1) - 3 ; // [-20, 20]
 //		vy = rnd.nextInt((3 + 3 ) + 1) - 3 ; // [-20, 20]
-		
-		if(x + width > 1000) { 
-			x = 1000-width  ; 
+
+		if (x + width > 1000) {
+			x = 1000 - width;
 		}
-		if( x < 0 ) { 
-			x = 0   ; 
+		if (x < 0) {
+			x = 0;
 		}
-		if( y < 0 ) { 
-			y = 0   ; 
+		if (y < 0) {
+			y = 0;
 		}
-		if( y + height > 625 ) { 
-			y = 625 - height  ;
+		if (y + height > 625) {
+			y = 625 - height;
 		}
-		
-		
-		//call update to update the actually picture location
+
+		// call update to update the actually picture location
 		update();
-		
-		
-		
-		
+
 		g2.drawImage(img, tx, null);
 	}
+
 	/* update the picture variable locssssations */
 	private void update() {
 
 		tx.setToTranslation(x, y);
 		tx.scale(scale, scale);
-		 
-		
+
 	}
-	
+
 	private void init(double a, double b) {
 		tx.setToTranslation(a, b);
 		tx.scale(scale, scale);
 	}
-	
+
 	private Image getImage(String path) {
 		Image tempImage = null;
 		try {
@@ -155,7 +148,7 @@ public class Fish {
 		}
 		return tempImage;
 	}
-	
+
 //	public static void main(String[] args) {
 //		// TODO Auto-generated method stub
 //
@@ -165,4 +158,3 @@ public class Fish {
 //	}
 
 }
-
--- TritonHacks2022/src/Frame.java
+++ TritonHacks2022/src/Frame.java
@@ -1,163 +1,162 @@
 import java.awt.event.ActionListener;
-	import java.awt.event.KeyListener;
-	import java.awt.event.MouseListener;
-	import java.awt.image.BufferedImage;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
 import java.awt.BasicStroke;
 import java.awt.Color;
-	import java.awt.Dimension;
-	import java.awt.Font;
-	import java.awt.Graphics;
-	import java.awt.GridLayout;
-	import java.awt.event.ActionEvent;
-	import java.awt.event.KeyEvent;
-	import java.awt.event.MouseEvent;
-	import java.util.ArrayList;
-	import javax.swing.JFrame;
-	import javax.swing.JPanel;
-	import javax.swing.Timer;
-	import javax.swing.JPanel;
-	import java.awt.Graphics2D;
-	
-public class Frame extends JPanel implements ActionListener, MouseListener, KeyListener{
-	
-	Background background = new Background(0,0);
-	Fish test = new Fish(0,0);
-	String current = "Fish1"; 
-	ArrayList<Fish> fishes = new ArrayList<Fish>(); 
-	
-	
-	
-	
-	public void spawn() { 
-		fishes.add(new Fish(0,0));
+import java.util.Random;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+import javax.swing.JPanel;
+import java.awt.Graphics2D;
+
+public class Frame extends JPanel implements ActionListener, MouseListener, KeyListener {
+	Random rnd = new Random();
+	Background background = new Background(0, 0);
+
+	String current = "Fish1";
+
+	ArrayList<Fish> fishes = new ArrayList<Fish>();
+
+	public void spawn() {
+		fishes.add(new Fish(rnd.nextInt((1000) + 1), rnd.nextInt((625) + 1), 1));
 	}
-	
-	
-		public void paint(Graphics g) {
-			super.paintComponent(g);
-			Graphics2D g2 = (Graphics2D) g;
-			background.paint(g);
-			
-			
-			test.paint(g);
-			test.move();
-			
-			g2.setStroke(new BasicStroke(5));
-			g.setColor(Color.BLACK);
-			g.setFont(new Font("Fish", Font.BOLD, 20));
-			
-			g.drawString("Current Fish: " + current, 1050, 30 ); 
-			
-			
-			g.drawRect(1010, 50, 80, 50);
-			g.drawString("Fish1", 1020, 80);
-			
-			g.drawRect(1100, 50, 80, 50);
-			g.drawString("Fish2", 1110, 80);
-			
-			g.drawRect(1190, 50, 80, 50);
-			g.drawString("Fish3", 1200, 80);
-			
-			g.drawRect(1115, 120, 100, 50);
-			g.drawString("Spawn", 1130, 150);
-		}
-	
-		
 
-		public static void main(String[] arg) {
-			Frame f = new Frame();
-			
-		}
-		
-		
-		public Frame() {
-			
-			JFrame f = new JFrame("Temp");
-			f.setSize(new Dimension(1520,700));
-			f.setBackground(Color.blue);
-			f.add(this);
-			f.setResizable(false);
-			f.setLayout(new GridLayout(1,2));
-			f.addMouseListener(this);
-			f.addKeyListener(this);
-			Timer t = new Timer(16, this);
-			t.start();
-			f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			f.setVisible(true);
-			
-		}
-		
-		
-		
-		@Override
-		public void mouseClicked(MouseEvent arg0) {
-			int mx = (int) arg0.getX();
-			int my = (int) arg0.getY();
-			 System.out.println(mx + " " + my);
-			 
-			 if(mx >= 1010 && mx < 1100 && my >= 80 && my <= 140 ) { 
-				 current = "Fish1";
-			 }
-			 
-			 if(mx >= 1100 && mx < 1190 && my >= 80 && my <= 140 ) { 
-				 current = "Fish2";
-			 }
-			 
-			 if(mx >= 1190 && mx < 1280 && my >= 80 && my <= 140 ) { 
-				 current = "Fish3";
-			 }
-			 
-			 
+	public void paint(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D) g;
+		background.paint(g);
+
+		for (int i = 0; i < fishes.size(); i++) {
+			fishes.get(i).paint(g);
+			fishes.get(i).move();
+
 		}
 
-		@Override
-		public void mouseEntered(MouseEvent arg0) {
-			
-		}
+		g.setColor(Color.BLACK);
+		g.setFont(new Font("Fish", Font.BOLD, 20));
 
-		@Override
-		public void mouseExited(MouseEvent arg0) {
-			
-		}
+		g.drawString("Goldfish Population: ", 1010, 40);
 
-		@Override
-		public void mousePressed(MouseEvent arg0) {
-		
-		}
+		g.drawString("Tropical Fish Population: ", 1010, 70);
+		g.drawString("Tropical Fish Kills: ", 1010, 90);
 
-		@Override
-		public void mouseReleased(MouseEvent arg0) {
-			// TODO Auto-generated method stub
-			
-		}
+		g.drawString("Shark Population: ", 1010, 120);
+		g.drawString("Shark Kills: ", 1010, 140);
+		g2.setStroke(new BasicStroke(5));
 
-		@Override
-		public void actionPerformed(ActionEvent arg0) {
-			// TODO Auto-generated method stub
-			repaint();
-		}
+		// g.drawString("Current Fish: " + current, 1050, 30 );
 
-		@Override
-		public void keyPressed(KeyEvent arg0) {
-			
-			
-		}		
-		//bathe them in iron
-		
+//			g.drawRect(1010, 50, 80, 50);s
+//			g.drawString("Fish1", 1020, 80);
+//			
+//			g.drawRect(1100, 50, 80, 50);
+//			g.drawString("Fish2", 1110, 80);
+//			
+//			g.drawRect(1190, 50, 80, 50);
+//			g.drawString("Fish3", 1200, 80);
+//			
+//			g.drawRect(1115, 120, 100, 50);
+//			g.drawString("Spawn", 1130, 150);
+	}
 
-		@Override
-		public void keyReleased(KeyEvent arg0) {
-		
-		}
-
-		@Override
-		public void keyTyped(KeyEvent arg0) {
-			
-			
-		}
-
-
+	public static void main(String[] arg) {
+		Frame f = new Frame();
 
 	}
 
+	public Frame() {
+		fishes.add(new Fish(rnd.nextInt((1000) + 1), rnd.nextInt((625) + 1), 1));
+		fishes.add(new Fish(rnd.nextInt((1000) + 1), rnd.nextInt((625) + 1), 1));
 
+		JFrame f = new JFrame("Temp");
+		f.setSize(new Dimension(1520, 700));
+		f.setBackground(Color.blue);
+		f.add(this);
+		f.setResizable(false);
+		f.setLayout(new GridLayout(1, 2));
+		f.addMouseListener(this);
+		f.addKeyListener(this);
+		Timer t = new Timer(16, this);
+		t.start();
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setVisible(true);
+
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		int mx = (int) arg0.getX();
+		int my = (int) arg0.getY();
+		System.out.println(mx + " " + my);
+
+		if (mx >= 1010 && mx < 1100 && my >= 80 && my <= 140) {
+			current = "Fish1";
+		}
+
+		if (mx >= 1100 && mx < 1190 && my >= 80 && my <= 140) {
+			current = "Fish2";
+		}
+
+		if (mx >= 1190 && mx < 1280 && my >= 80 && my <= 140) {
+			current = "Fish3";
+		}
+		if (mx >= 1120 && mx < 1220 && my >= 150 && my <= 200) {
+			spawn();
+		}
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		repaint();
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+
+	}
+	// bathe them in iron
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+
+	}
+
+}
